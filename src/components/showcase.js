b43a3ba89a360c77980aca3b4dcd173fa5f4fa95
Jennifer Frutos <jennifer.frutos92@gmail.com>
2022-09-29 13:36:55 -0700

    icons and links

--- src/components/showcase.js
+++ src/components/showcase.js
@@ -7,27 +7,33 @@ const Showcase = () => {
         <>
             <h2 id="showcase">Some Creations </h2>
             <div className="container showcase" >
-                <Card className="showcase-card" >
-                    <Card.Body>
-                        <Card.Title>Flora <IoOpenOutline /></Card.Title>
-                        <Card.Subtitle>React | Bootstrap</Card.Subtitle>
-                        <Card.Text>Mock e-commerce site for a plants. Used simple design and relevant color scheme</Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card className="showcase-card" >
-                    <Card.Body>
-                        <Card.Title>The Glass Shop <IoOpenOutline /></Card.Title>
-                        <Card.Subtitle>React | Bootstrap | CSS</Card.Subtitle>
-                        <Card.Text>Mock site for a small business to showcase products and potentially increase customer base.</Card.Text>
-                    </Card.Body>
-                </Card>
-                <Card className="showcase-card" >
-                    <Card.Body>
-                        <Card.Title>Misc-Apps <IoOpenOutline /></Card.Title>
-                        <Card.Subtitle>React | JavaScript | CSS | Bootstrap</Card.Subtitle>
-                        <Card.Text>Collection of apps made for fun: Retro Calculator, Random Activiy Generator, To-Do list</Card.Text>
-                    </Card.Body>
-                </Card>
+                <a href="https://floramocksite.netlify.app" target="_blank" rel="noreferrer">
+                    <Card className="showcase-card" >
+                        <Card.Body>
+                            <Card.Title>Flora <IoOpenOutline /></Card.Title>
+                            <Card.Subtitle>React | Bootstrap</Card.Subtitle>
+                            <Card.Text>Mock e-commerce site for a plants. Used simple design and relevant color scheme</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </a>
+                <a href="https://genericshop.netlify.app" target="_blank" rel="noreferrer">
+                    <Card className="showcase-card" >
+                        <Card.Body>
+                            <Card.Title>The Glass Shop <IoOpenOutline /></Card.Title>
+                            <Card.Subtitle>React | Bootstrap | CSS</Card.Subtitle>
+                            <Card.Text>Mock site for a small business to showcase products and potentially increase customer base.</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </a>
+                <a href="https://miscapps.netlify.app" target="_blank" rel="noreferrer">
+                    <Card className="showcase-card" >
+                        <Card.Body>
+                            <Card.Title>Misc-Apps <IoOpenOutline /></Card.Title>
+                            <Card.Subtitle>React | JavaScript | CSS | Bootstrap</Card.Subtitle>
+                            <Card.Text>Collection of apps made for fun: Retro Calculator, Random Activiy Generator, To-Do list</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </a>
             </div>
 
         </>
