96f9a97dd4874961ea22ef8fffe8129ae1bdc6a1
Jennifer Frutos <jenniferfrutosdev@gmail.com>
2022-11-15 11:58:03 -0800

    updated bio

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -14,7 +14,7 @@ const About = () => {
                     <img src={require('../assets/selfie.jpg')} alt="rainbow" className="about-img" />
                 </Col>
                 <Col xs lg="5">
-                    <p><IoSparklesSharp /><span>F</span>ueled by  COFFEE , I recently completed a Full Stack bootcamp with Nucamp.I left my management position to pursue a career in Web Development. Here to make pretty things for the web, but my spare time includes short-lived hobbies and video games. Always tinkering and eager to learn something new. For those asking, I am an Aquarius <IoSparklesSharp /></p>
+                    <p><IoSparklesSharp /><span>F</span>ueled by coffee, I recently completed a Full Stack bootcamp with Nucamp. Here to make pretty things for the web, but my spare time includes an array of hobbies and video games. Always tinkering and eager to learn something new. <IoSparklesSharp /></p>
                 </Col>
             </Row>
             
